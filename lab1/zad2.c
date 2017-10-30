9f4531b93d2ff681d22c246276f94bd48fa39500
Paweł Niczewski <pniczewski@sigma-ng.inf.ug.edu.pl>
2017-10-30 23:49:11 +0100

    zadania

--- lab1/zad2.c
+++ lab1/zad2.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
- int main() {
-   int a, b, c;
-   int suma, roznica, iloczyn, iloraz;
-   double dzielenie;
+ int main()
 
-   printf ("podaj a\n" );
-   scanf ("%d", &a );
-   printf ("podaj b\n" );
-   scanf ("%d", &b );
-   printf("wybierz metode:\n  1.dodawanie\n 2.odejmowanie\n 3.mnozenie\n 4.dzielenie\n );
-   scanf ("%d",&c );
+{
+
+   int a;
+   int b;
+   int c;
+   int suma;
+   int roznica;
+   int iloczyn; 
+   double iloraz;
+
+   printf("podaj pierwszą liczbę a\n" );
+   scanf("%d", &a );
+   printf("podaj drugą liczbę b\n" );
+   scanf("%d", &b );
+   printf("wybierz metode:\n 1.dodawanie\n 2.odejmowanie\n 3.mnozenie\n 4.dzielenie\n" );
+   scanf("%d",&c );
    suma= a + b;
    roznica = a - b;
    iloczyn = a * b ;
-   iloraz = a / b ;
-   if(c==1){
+   iloraz =(double)a / (double)b ;
 
-     printf("suma: %d\n,suma");
-   }
+	if(c==1)
+{
 
+   printf("suma: %d\n", suma); 
 
+}
+	if(c==2)
+{
+ 
+   printf("roznica: %d\n", roznica);
+
+}
+	if(c==3) 
+{
+
+   printf("iloczyn: %d\n", iloczyn);
+
+}
+	if(c==4){
+
+   printf("iloraz: %f\n", iloraz);
+}
 
   return 0;
 }
